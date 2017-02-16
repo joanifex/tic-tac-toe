40838b489968f04bb0a58897f3d249f788f75152
Ian Wright <wright.ianb@gmail.com>
2017-02-16 08:14:45 -0700

    adds random computer moves

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,22 +5,39 @@ $(document).ready(function(){
     [1,4,7], [2,5,8], [3,6,9], // verticals
     [1,5,9], [3,5,7]           // diagonals
   ];
+  var playerChoices = [],
+      computerChoices = [];
 
-  var playerChoices = [];
-  var computerChoices = [];
-
-  function computerTurn(){};
+  function computerTurn(){
+    //refactor selection function
+    if ( $('.box-5').hasClass('box-open') ){
+      $('.box-5').html('<p class="text">o</p>');
+      $('.box-5').removeClass('box-open');
+      $('.box-5').addClass('box-closed');
+      computerChoices.push($('.box-5').data('boxNumber'));
+    } else {
+      var random = Math.floor( Math.random() * $(".box-open").length);
+      var $button = $(".box-open").eq(random);
+      $button.html('<p class="text">o</p>');
+      $button.removeClass('box-open');
+      $button.addClass('box-closed');
+    }
+  };
 
   function win(choices){
     if ( choices.length >= 3 ) {
       return winningCombinations.some(function(combination){
         return combination.every(function(number){
-          return playerChoices.indexOf(number) > (-1);
+          return playerChoices.indexOf(number) > - 1;
         });
       });
     } else {
       return false;
     }
+  };
+
+  function checkTie(){
+
   };
 
   $('.btn').each(function( index ){
